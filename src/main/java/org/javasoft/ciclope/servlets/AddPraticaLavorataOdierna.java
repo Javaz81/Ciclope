1be167f806e6d8e47c8542ea5275913f6d323540
andrea <andrea@10.0.9.17>
2016-07-25 18:05:45 +0200

    Aggiunto il pulsante "aggiungi pratica lavorata" nella lista delle pratiche giornalierre lavorate. Deve essere corretta la query sql per visualizzare tutte le pratiche correnti non lavorate nella giornata in maniera corretta E poi fare correttamente il refresh della lista delle pratiche seguite una volta aggiunta una pratica e ritornati nella pagina della lista delle pratiche seguite stessa.

---
+++ src/main/java/org/javasoft/ciclope/servlets/AddPraticaLavorataOdierna.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasoft.ciclope.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.javasoft.ciclope.persistence.HibernateUtil;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author andrea
+ */
+@WebServlet(name = "AddPraticaLavorataOdierna", urlPatterns = {"/AddPraticaLavorataOdierna"})
+public class AddPraticaLavorataOdierna extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            SessionFactory sf = HibernateUtil.getSessionFactory();
+            Session s = sf.openSession();
+            Transaction t = s.getTransaction();
+            t.begin();
+            BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String json;
+            json = br.readLine();
+            Object obj = null;
+            JSONParser p = new JSONParser();
+            if (json != null) {
+                try {
+                    obj = p.parse(json);
+                } catch (ParseException ex) {
+                    Logger.getLogger(GetRifornimentiDaFare.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                t.rollback();
+                JSONObject jo = new JSONObject();
+                jo.put("result", "ko");
+                out.println(jo.toJSONString());
+                return;
+            }
+            String praticaId = (String) ((JSONObject) obj).get("praticaId");
+            String giornataJs = (String) ((JSONObject) obj).get("giornata");
+            String personaleId = (String) ((JSONObject) obj).get("personale");
+            String giornata =""; 
+            try {
+                giornata = new SimpleDateFormat("yyyy-MM-dd").format(new SimpleDateFormat("dd/MM/yyyy").parse(giornataJs));
+            } catch (java.text.ParseException ex) {
+                t.rollback();
+                JSONObject jo = new JSONObject();
+                jo.put("result", "ko");
+                out.println(jo.toJSONString());
+                return;
+            }
+
+            //Inserisci il materiale nella pratica ed aggiorna di una quantita.
+            Query q = s.createSQLQuery("INSERT INTO ciclope.orelavorate "
+                    + "(ore, personale, pratica, giornata) VALUES ('0','" + personaleId + "',"
+                    + " '" + praticaId + "', '" + giornata + "')");
+            int n_row = q.executeUpdate();
+            if (n_row != 1) {
+                t.rollback();
+                JSONObject jo = new JSONObject();
+                jo.put("result", "ko");
+                out.println(jo.toJSONString());
+                return;
+            }
+            t.commit();
+
+            JSONObject jo = new JSONObject();
+            jo.put("result", "ok");
+            out.println(jo.toJSONString());
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold
+}
---
+++ src/main/java/org/javasoft/ciclope/servlets/GetPraticheOdierneLavorabili.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasoft.ciclope.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.javasoft.ciclope.persistence.HibernateUtil;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author andrea
+ */
+@WebServlet(name = "GetPraticheOdierneLavorabili", urlPatterns = {"/GetPraticheOdierneLavorabili"})
+public class GetPraticheOdierneLavorabili extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            SessionFactory sf = HibernateUtil.getSessionFactory();
+            Session s = sf.openSession();
+            Transaction t = s.getTransaction();
+            t.begin();
+            BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String json;
+            json = br.readLine();
+            Object obj;
+            JSONParser p = new JSONParser();
+            if (json != null) {
+                try {
+                    obj = p.parse(json);
+                } catch (ParseException ex) {
+                    Logger.getLogger(GetRifornimentiDaFare.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            Query q = s.createSQLQuery("select ciclope.pratica.idPratica as praticaId,\n"
+                    + "ciclope.pratica.arrivo as arrivo,\n"
+                    + "ciclope.veicolo.marca as marca,\n"
+                    + "ciclope.veicolo.modello as modello,\n"
+                    + "ciclope.veicolo.targa as targa,\n"
+                    + "ciclope.veicolo.tipo as tipo\n"
+                    + "from ciclope.pratica\n"
+                    + "left join ciclope.veicolo on ciclope.pratica.Veicolo = ciclope.veicolo.idVeicolo\n"
+                    + "where ciclope.pratica.uscita is null\n"
+                    + "and ciclope.pratica.idPratica not in \n"
+                    + "(\n"
+                    + "select orelavorate.pratica\n"
+                    + "from orelavorate\n"
+                    + "where orelavorate.giornata = '2016-3-26'\n"
+                    + "and orelavorate.personale = 1\n"
+                    + ")");
+            List<Object[]> aicrecs = q.list();
+            t.commit();
+            JSONObject jo;
+            JSONArray array = new JSONArray();
+
+            for (Object[] ob : aicrecs) {
+                jo = new JSONObject();
+                jo.put("praticaId", ob[0].toString());
+                jo.put("arrivo", ob[1].toString());
+                jo.put("marca", ob[2].toString());
+                jo.put("modello", ob[3].toString());
+                jo.put("targa", ob[4] == null ? "Speciale" : ob[4].toString());
+                jo.put("tipo", ob[5].toString());
+                array.add(jo);
+            }
+            out.println(array.toJSONString());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
