9f0385d2aa4960dc349746fc4490cb23cf5a4aac
andrea <andrea@192.168.1.131>
2016-10-14 22:45:43 +0200

    Parte di magazzino quasi completata. Manca la visualizzazione in decimale delle quantità di materiali in Rifornimenti e Approvvigionamenti. Manca anche l'inserimento in decimale in queste categorie.

---
+++ src/main/java/org/javasoft/ciclope/servlets/GetRifornimenti.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javasoft.ciclope.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.javasoft.ciclope.persistence.Articolo;
+import org.javasoft.ciclope.servlets.utils.SessionUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author andrea
+ */
+@WebServlet(name = "GetRifornimenti", urlPatterns = {"/GetRifornimenti"})
+public class GetRifornimenti extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {            
+            BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String json;
+            json = br.readLine();
+            
+            Session s = SessionUtils.getCiclopeSession();
+            Transaction t= s.getTransaction();
+            t.begin();
+            Query q = s.createSQLQuery("select * from articolo").addEntity(Articolo.class);
+            List<Articolo> aicrecs = q.list();
+            t.commit();
+            JSONObject jo;
+            JSONArray array = new JSONArray();
+
+            for (Articolo ob : aicrecs) {
+                jo = new JSONObject();
+                jo.put("codice", ob.getIdArticolo());
+                jo.put("descrizione", ob.getDescrizione());
+                jo.put("rimanenza", ob.getScortaRimanente());
+                jo.put("scorta_minima",ob.getScortaRimanente());
+                jo.put("unita_di_misura", ob.getUnitaDiMisura());
+                jo.put("approvvigionamento",ob.getApprovvigionamento());
+                jo.put("color", ob.getScortaRimanente()-ob.getScortaMinima()<0?"red":"blue");
+                array.add(jo);
+            }
+            out.println(array.toJSONString());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
